f446d9ba9ae5fc54d270379979599743690a5371
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-27 20:32:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+require 'pry'
+class School
+  def initialize(name)
+    @name = name 
+    roster
+  end 
+  def roster
+      @roster_hash = {}
+  end
+  
+  def add_student(name, grade)
+    @roster_hash[grade] = []
+    @roster_hash[grade] << name
+  end 
+  
+end 
