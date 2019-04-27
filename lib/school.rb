93131cf2781249e9713573beee52a2f40ab13f59
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-27 21:07:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,19 @@
 require 'pry'
 class School
+  #@@roster = Hash.new
   def initialize(name)
     @name = name 
     roster
   end 
   
   def roster
-    @roster_hash = Hash.new
+    @@roster = Hash.new
   end
   
   def add_student(name, grade)
     #roster = {}
-    roster[grade] = []
-    roster[grade] << name
-    roster
+    @@roster[grade] = []
+    @@roster[grade] << name
+    @@roster
   end
 end 
